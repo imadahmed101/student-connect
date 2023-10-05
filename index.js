7a1322e1534a6dddd695b66ca7052450a13931d3
Imad Ahmed <imadahmed101@gmail.com>
2023-10-05 14:40:16 -0400

    updated index file to be a basic express app for testing deployment

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
+// const express = require('express')
+// const app = express()
+// app.use(express.json())
+// const env = require('dotenv/config')
+// const mongoose = require('mongoose')
+// const cors = require('cors')
+
+// const studentRoute = require('./routes/student')
+// const postRoute = require('./routes/post')
+// const commentRoute = require('./routes/comment')
+
+// const PORT = process.env.PORT || 80
+// const DBCONNECTION = process.env.DBCONNECTION
+
+// app.use(cors())
+// app.use('/', (studentRoute))
+// app.use('/post', (postRoute))
+// app.use('/comment', (commentRoute))
+
+// mongoose.connect(DBCONNECTION)
+// .then(() => {app.listen((PORT), console.log (`Server running on port ${PORT}`))})
+// .catch((error) => console.log(error.message))
+
 const express = require('express')
 const app = express()
-app.use(express.json())
-const env = require('dotenv/config')
-const mongoose = require('mongoose')
-const cors = require('cors')
+const port = process.env.PORT || 80
 
-const studentRoute = require('./routes/student')
-const postRoute = require('./routes/post')
-const commentRoute = require('./routes/comment')
+app.get('/', (req, res) => {
+    res.send('YESSIR')
+})
 
-const PORT = process.env.PORT || 80
-// const PORT = process.env.PORT
-const DBCONNECTION = process.env.DBCONNECTION
-
-app.use(cors())
-app.use('/', (studentRoute))
-app.use('/post', (postRoute))
-app.use('/comment', (commentRoute))
-
-mongoose.connect(DBCONNECTION)
-.then(() => {app.listen((PORT), console.log (`Server running on port ${PORT}`))})
-.catch((error) => console.log(error.message))
+app.listen(port, () => {
+    console.log('server running')
+})
