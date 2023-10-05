cea2be5900ea412376882188fe935df38d46e786
Imad Ahmed <imadahmed101@gmail.com>
2023-10-05 15:03:08 -0400

    updated index and restored it to previous and going to add environment variables directly into azure

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-// const express = require('express')
-// const app = express()
-// app.use(express.json())
-// const env = require('dotenv/config')
-// const mongoose = require('mongoose')
-// const cors = require('cors')
-
-// const studentRoute = require('./routes/student')
-// const postRoute = require('./routes/post')
-// const commentRoute = require('./routes/comment')
-
-// const PORT = process.env.PORT || 80
-// const DBCONNECTION = process.env.DBCONNECTION
-
-// app.use(cors())
-// app.use('/', (studentRoute))
-// app.use('/post', (postRoute))
-// app.use('/comment', (commentRoute))
-
-// mongoose.connect(DBCONNECTION)
-// .then(() => {app.listen((PORT), console.log (`Server running on port ${PORT}`))})
-// .catch((error) => console.log(error.message))
-
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 80
+app.use(express.json())
+const env = require('dotenv/config')
+const mongoose = require('mongoose')
+const cors = require('cors')
 
-app.get('/', (req, res) => {
-    res.send('YESSIR')
-})
+const studentRoute = require('./routes/student')
+const postRoute = require('./routes/post')
+const commentRoute = require('./routes/comment')
 
-app.listen(port, () => {
-    console.log('server running')
-})
+const PORT = process.env.PORT || 80
+const DBCONNECTION = process.env.DBCONNECTION
+
+app.use(cors())
+app.use('/', (studentRoute))
+app.use('/post', (postRoute))
+app.use('/comment', (commentRoute))
+
+mongoose.connect(DBCONNECTION)
+.then(() => {app.listen((PORT), console.log (`Server running on port ${PORT}`))})
+.catch((error) => console.log(error.message))
+
+// const express = require('express')
+// const app = express()
+// const port = process.env.PORT || 80
+
+// app.get('/', (req, res) => {
+//     res.send('YESSIR')
+// })
+
+// app.listen(port, () => {
+//     console.log('server running')
+// })
